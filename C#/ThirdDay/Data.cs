cea91d01804f31559eeb1f8796f85c72333521f2
itsnegative <officiallynegative@protonmail.com>
2022-01-20 11:56:36 -0500

    Added comments

--- C#/ThirdDay/Data.cs
+++ C#/ThirdDay/Data.cs
@@ -3,14 +3,39 @@ namespace DataFunction
     public class Data
     {
 
+        /*
+         * Represents the cost of a big mac.
+         */
         public static int BIG_MAC_COST = 5;
+
+        /*
+         * Represents the cost of a beyond meat burger.
+         */
         public static int BEYOND_COST = 7;
 
+        /*
+         * Represents if the user has ordered anything.
+         */
         public bool _ordered { get; set; }
+
+        /*
+         * Represents the amount of cash the user has.
+         */
         public int _cash { get; set; }
+
+        /*
+         * Represents the total cart balance.
+         */
         public int _balance { get; set; }
+
+        /*
+         * An array that stores how many burgers a user has ordered.
+         */
         public int[] _amountOrdered = new int[2];
     
+        /*
+         * Initalize all needed variables.
+         */
         public void init()
         {
             _ordered = false;
@@ -23,6 +48,9 @@ namespace DataFunction
             _amountOrdered[1] = 0;
         }
 
+        /*
+         * Reset the user's cart.
+         */
         public void reset()
         {
             _amountOrdered[0] = 0;
@@ -31,12 +59,18 @@ namespace DataFunction
             _ordered = false;
         }
 
+        /*
+         * Remove a single big mac from the cart.
+         */
         public void removeBigMac()
         {
             _amountOrdered[0] = _amountOrdered[0] - 1;
             Console.WriteLine("You now have "+ _amountOrdered[0] +" Big Mac(s) in your cart.");
         }
 
+        /*
+         * Remove a single beyond meat burger from the cart.
+         */
         public void removeBeyond()
         {
             _amountOrdered[0] = _amountOrdered[0] - 1;
